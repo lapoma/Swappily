5ad1e53132208a763834e4fca5df1ad98578d325
lapoma <marta.lapo@studenti.unitn.it>
2025-07-16 14:41:08 +0200

    Update exchanges.js

--- app/exchanges.js
+++ app/exchanges.js
@@ -14,40 +14,15 @@ const authenticateJWT = (req, res, next) => {
         return res.status(403).json({ error: 'Token mancante' });
     }
 
-//     jwt.verify(authHeader, process.env.SUPER_SECRET, (err, user) => {
-//         if (err) {
-//             return res.status(401).json({ error: 'Token non valido' });
-//         }
-//         req.userId = user._id;
-//         next();
-//     });
-// };
+    jwt.verify(authHeader, process.env.SUPER_SECRET, (err, user) => {
+        if (err) {
+            return res.status(401).json({ error: 'Token non valido' });
+        }
+        req.userId = user.id;
+        next();
+    });
+};
 
-function tokenChecker(req, res, next) {
-  // Leggi prima dall'header 'token', poi da query.token
-  const token = req.headers['token'] || req.query.token;
-  console.log('[tokenChecker] token:', token);
-
-  if (!token) {
-    console.log('[tokenChecker] no token provided');
-    return res.status(401).json({ message: 'No token provided.' });
-  }
-
-  jwt.verify(token, process.env.SUPER_SECRET, (err, decoded) => {
-    if (err) {
-        return res.status(403).json({
-            success: false,
-            message: 'Failed to authenticate token.'
-        });
-    }
-    // Attach the decoded user id to the request object
-    req.loggedUser = decoded;
-    console.log(loggedUser)
-    req.userId = decoded._id;
-    console.log(req.userId)
-    next();
-});
-}
 
 // POST /exchange/listing/{listingId}
 router.post('/listing/:listingId', async (req, res) => {
