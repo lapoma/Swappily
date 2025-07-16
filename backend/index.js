4e8de466e1b173b132a0069ac625e2be96b5042c
benny <benedettabicego4@gmail.com>
2025-07-17 01:12:03 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,15 @@ const mongoose = require('mongoose'); // Libreria per MongoDB
 const app = require('./app/app.js');
 
 
-const port = process.env.PORT;
-if (!port) {
-  throw new Error('PORT is not defined in environment variables!');
-}
+const PORT = process.env.PORT || 8080;
 
 app.locals.db = mongoose.connect( process.env.DB_URL )
 .then ( () => {
     
     console.log("Connected to Database");
     
-    app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
     });
     
 });
