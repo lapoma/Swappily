aa72894c23b57c12edfc46f2f0a99542862b8855
benny <benedettabicego4@gmail.com>
2025-07-17 00:58:15 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const app = require('./app/app.js');
 
 
 const port = process.env.PORT;
+if (!port) {
+  throw new Error('PORT is not defined in environment variables!');
+}
 
 app.locals.db = mongoose.connect( process.env.DB_URL )
 .then ( () => {
